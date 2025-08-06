e6111d07082332a2ec710ba863cc98109451883b
Leonardo Bishop <me@leonardobishop.net>
2025-08-06 22:41:15 +0100

    Remove PKGBUILD and install script

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#if ! [ rustc --version > /dev/null 2>&1 ]; then
-#    sudo pacman -S rustup || exit $?
-#    rustup default stable || exit $?
-#fi
-makepkg -f || exit $?
-sudo pacman -U dotfiles-installer*.pkg.tar*
